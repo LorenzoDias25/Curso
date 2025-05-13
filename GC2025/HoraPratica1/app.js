9e20237d2d0c461f6cd17dc59953da087875e00a
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-05-13 16:57:24 -0300

    yes

--- GC2025/HoraPratica1/app.js
+++ GC2025/HoraPratica1/app.js
@@ -5,6 +5,22 @@ function alterarTexto(tag, texto){
 
 alterarTexto('h1', "Hora do desafio");
 
-function alertClick(){
+function buttonClick(){
   console.log("O botão foi clicado");
+}
+
+function alertClick(){
+  alert("Eu amo JS");
+}
+
+function verificarClick(){
+  var cidade = prompt("Digite o nome de uma cidade do Brasil: ");
+  alert("Estive em " + cidade + " e lembrei de você !");
+}
+
+function somaClick(){
+  var n1 = prompt("Digite um número: ");
+  var n2 = prompt("Digite outro número: ");
+
+  alert("A soma de " + n1 + " + " + n2 + " é: " + (parseInt(n1) + parseInt(n2)));
 }
