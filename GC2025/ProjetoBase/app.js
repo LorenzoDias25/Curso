06f6de9b479ff6f8200f01a0487d6aa063b7bd78
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-05-14 16:03:47 -0300

    hahahhaah

--- GC2025/ProjetoBase/app.js
+++ GC2025/ProjetoBase/app.js
@@ -1,4 +1,7 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+let palavraTentativa;
+let mensagemTentativas;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,7 +13,33 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    
+    if (chute == numeroSecreto){
+        exibirTextoNaTela("h1", "Acertou");
+        palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela("h1", "Errou, tente novamente");
+            palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+            mensagemTentativas = `O número secreto é menor. Tentativas: ${tentativas} ${palavraTentativa}`
+            exibirTextoNaTela("p", mensagemTentativas);
+        } else {
+            exibirTextoNaTela("h1", "Errou, tente novamente");
+            palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+            mensagemTentativas = `O número secreto é maior. Tentativas: ${tentativas} ${palavraTentativa}`
+            exibirTextoNaTela("p", mensagemTentativas);
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function limparCampo(){
+    chute = document.querySelector("input");
+    chute.value = '';
 }
 
 function gerarNumeroAleatorio() {
