b1c16c9d2ab15ca066c6d9220faa535846976561
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-05-17 11:15:27 -0300

    Update app.js

--- GC2025/LogicaDeProgramacao2/AlugarGames/js/app.js
+++ GC2025/LogicaDeProgramacao2/AlugarGames/js/app.js
@@ -1,15 +1,27 @@
-function alterarStatus(id){
+function alterarStatus(id) {
   let gameClicado = document.getElementById(`game-${id}`);
-  let imagem = gameClicado.querySelector('.dashboard__item__img');
-  let botao = gameClicado.querySelector('.dashboard__item__button');
+  let imagem = gameClicado.querySelector(".dashboard__item__img");
+  let botao = gameClicado.querySelector(".dashboard__item__button");
 
-  if (imagem.classList.contains('dashboard__item__img--rented')){
-    imagem.classList.remove('dashboard__item__img--rented');
-    botao.classList.remove('dashboard__item__button--return');
-    botao.textContent = "Alugar";
+  if (imagem.classList.contains("dashboard__item__img--rented")) {
+    if (confimarDevolucao()) {
+      imagem.classList.remove("dashboard__item__img--rented");
+      botao.classList.remove("dashboard__item__button--return");
+      botao.textContent = "Alugar";
+    }
   } else {
-    imagem.classList.add('dashboard__item__img--rented');
+    imagem.classList.add("dashboard__item__img--rented");
     botao.textContent = "Devolver";
-    botao.classList.add('dashboard__item__button--return');
+    botao.classList.add("dashboard__item__button--return");
+  }
+}
+
+function confimarDevolucao() {
+  let text = prompt("Digite S para confimar ou N para cancelar");
+
+  if (text.charAt(0) == "S") {
+    return true;
+  } else {
+    return false;
   }
 }
