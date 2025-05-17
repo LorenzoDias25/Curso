c09657683ad3d0b9549a467d7a84181efe75862e
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-05-17 13:13:00 -0300

    Update app.js

--- GC2025/LogicaDeProgramacao2/AlugarGames/js/app.js
+++ GC2025/LogicaDeProgramacao2/AlugarGames/js/app.js
@@ -1,3 +1,5 @@
+let quantidade = 1;
+
 function alterarStatus(id) {
   let gameClicado = document.getElementById(`game-${id}`);
   let imagem = gameClicado.querySelector(".dashboard__item__img");
@@ -5,14 +7,25 @@ function alterarStatus(id) {
 
   if (imagem.classList.contains("dashboard__item__img--rented")) {
     if (confimarDevolucao()) {
+      quantAlugados(false)
       imagem.classList.remove("dashboard__item__img--rented");
       botao.classList.remove("dashboard__item__button--return");
       botao.textContent = "Alugar";
     }
   } else {
+    quantAlugados(true)
     imagem.classList.add("dashboard__item__img--rented");
     botao.textContent = "Devolver";
     botao.classList.add("dashboard__item__button--return");
+  }
+  console.log(quantidade);
+}
+
+function quantAlugados(verify){
+  if (verify){
+    quantidade += 1;
+  } else{
+    quantidade += -1;
   }
 }
 
