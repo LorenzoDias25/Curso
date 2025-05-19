ee5e5d61ff88a270d212afe1b46de8d9c5e8edb4
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-05-19 14:54:16 -0300

    aa

--- GC2025/LogicaDeProgramacao2/CarrinhoDeCompras/js/app.js
+++ GC2025/LogicaDeProgramacao2/CarrinhoDeCompras/js/app.js
@@ -7,25 +7,27 @@ function adicionar() {
   let nomeProduto = produto.split("-")[0];
   let valorUnitario = produto.split("R$")[1];
   let quantidade = document.getElementById("quantidade").value;
-  if (verificaQuant(quantidade)){
-  let preco = quantidade * valorUnitario;
-  let carinho = document.getElementById("lista-produtos");
-  carinho.innerHTML =
-    carinho.innerHTML +
-    `<section class="carrinho__produtos__produto">
+  if (verificaQuant(quantidade)) {
+    
+    let preco = quantidade * valorUnitario;
+    let carinho = document.getElementById("lista-produtos");
+    mudarTexto(preco, quantidade);
+    carinho.innerHTML =
+      carinho.innerHTML +
+      `<section class="carrinho__produtos__produto">
           <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$1400</span>
         </section>`;
-  totalGeral = totalGeral + preco;
-  let campoTotal = document.getElementById("valor-total");
-  campoTotal.textContent = `R$ ${totalGeral}`;
-  document.getElementById("quantidade").value = 0;
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById("valor-total");
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById("quantidade").value = 0;
   } else {
     alert("Quantidade inválida !!!");
   }
 }
 
-function verificaQuant(quantidade){
-  if(quantidade <= 0){
+function verificaQuant(quantidade) {
+  if (quantidade <= 0) {
     return false;
   } else {
     return true;
@@ -36,4 +38,12 @@ function limpar() {
   totalGeral = 0;
   document.getElementById("lista-produtos").innerHTML = "";
   document.getElementById("valor-total").innerHTML = "R$ 0";
+}
+
+//Hora da pratica
+
+function mudarTexto(preco,quantidade) {
+  let teste = document.getElementById("teste");
+  let final = preco * quantidade;
+  teste.textContent = `Preço: ${preco} Quantidade: ${quantidade} Total: R$ ${final}`;
 }
