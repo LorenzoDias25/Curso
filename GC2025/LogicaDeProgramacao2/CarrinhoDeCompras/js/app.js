1a99361abef4207d5cfb585597368be9357247c8
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-05-19 15:13:25 -0300

    ae

--- GC2025/LogicaDeProgramacao2/CarrinhoDeCompras/js/app.js
+++ GC2025/LogicaDeProgramacao2/CarrinhoDeCompras/js/app.js
@@ -11,7 +11,9 @@ function adicionar() {
     
     let preco = quantidade * valorUnitario;
     let carinho = document.getElementById("lista-produtos");
-    mudarTexto(preco, quantidade);
+    //mudarTexto(preco, quantidade);
+    //separarString("COMO VOCE, TA");
+    separarNumeros("1,2,3,4,5,6,7,8,9,10");
     carinho.innerHTML =
       carinho.innerHTML +
       `<section class="carrinho__produtos__produto">
@@ -48,6 +50,18 @@ function mudarTexto(preco,quantidade) {
   teste.textContent = `Preço: ${preco} Quantidade: ${quantidade} Total: R$ ${final}`;
 }
 
-function separarString(){
-  
+function separarString(frase){
+  let p1 = frase.split(',')[0];
+  let p2 = frase.split(',')[1];
+
+  teste.textContent = `Frase: ${frase} Parte 1: ${p1} Parte 2: ${p2}`;
+}
+
+function separarNumeros(frase){
+  let numero = frase.split(',');
+  console.log("Numeros separados: ");
+  numero.forEach(num => {
+    console.log(num.trim());
+  });
+
 }
